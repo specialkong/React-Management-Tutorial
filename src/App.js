f4265a989709cfa55b47f7e5d437b2c6f8832e5b
purue <specialkong@daum.net>
2019-07-21 16:19:54 +0900

    first commit

--- src/App.js
+++ src/App.js
@@ -1,18 +1,16 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Hello React
-        </p>          
-      </header>
-    </div>
-  );
+class App extends Component {
+render() {
+return (
+<div className="gray-background">
+<img src={logo} alt="logo" />
+<h2>Let's develop management system!</h2>
+</div>
+);
+}
 }
 
 export default App;
